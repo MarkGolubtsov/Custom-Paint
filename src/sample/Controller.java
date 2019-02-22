3e53c576254507aeb618a9ece0af61d32c0bbc6f
Mark <glmarkgl4@gmail.com>
2019-02-22 23:16:59 +0300

    Rectangle

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,9 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import sample.Obj.Line;
+import sample.Obj.Rectangle;
 
 
 public class Controller {
@@ -14,12 +16,22 @@ public class Controller {
     Button Line1;
 
     Line line= new Line();
+    Rectangle rectangle=new Rectangle();
+
+    double x1,y1,x2,y2;
 
 
-    public void DrawLine(){
 
-        line.Draw(MainCanvas,20,20,50,50);
+    public void handle(MouseEvent mouseEvent) {
+           x1 =mouseEvent.getSceneX();
+           y1=mouseEvent.getSceneY()-77;
+        }
 
+    public void handle1(MouseEvent mouseEvent) {
+
+            x2 = mouseEvent.getSceneX();
+            y2 = mouseEvent.getSceneY()-77;
+            rectangle.Draw(MainCanvas, x1, y1, x2, y2);
 
     }
-}
+    }
